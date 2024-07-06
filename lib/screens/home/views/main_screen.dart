8c1a17d00a81316893913023b45f444f7a6eb416
Ayush Maharjan <Ayush.maharjan399@gmail.com>
2024-07-06 17:16:07 +0545

    Commit

--- lib/screens/home/views/main_screen.dart
+++ lib/screens/home/views/main_screen.dart
@@ -58,36 +58,148 @@ class MainScreen extends StatelessWidget {
                 ),
               ],
             ),
-            const SizedBox(height: 12),
+            const SizedBox(height: 24),
             Container(
               width: MediaQuery.of(context).size.width,
-              height:MediaQuery.of(context).size.width / 2,
+              height: MediaQuery.of(context).size.width / 2,
               decoration: BoxDecoration(
                 color: Theme.of(context).colorScheme.primary,
-                borderRadius:BorderRadius.circular(24)
+                borderRadius: BorderRadius.circular(24),
               ),
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text('Total Balance'),
-                  Text('Rs.40000'),
-                  Row(
-                    children: [
-                      Container(
-                        width: 20,
-                        height: 20,
-                        decoration: BoxDecoration(
-                            color: Colors.grey.shade500,
-                          shape: BoxShape.circle
+                  Text(
+                    'Total Balance',
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.tertiary,
+                      fontSize: 16,
+                    ),
+                  ),
+                  SizedBox(height: 14),
+                  Text(
+                    'Rs.40000.00',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 12, horizontal: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        //for Income Section
+                        Row(
+                          children: [
+                            Container(
+                              width: 20,
+                              height: 20,
+                              decoration: BoxDecoration(
+                                color: Color(0xFFD68FE1),
+                                shape: BoxShape.circle,
+                              ),
+                              child: const Center(
+                                child: Icon(Icons.arrow_downward_rounded,
+                                    size: 12,
+                                  color: Colors.greenAccent,),
+                              ),
+                            ),
+                            SizedBox(width: 10),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  'Income',
+                                  style: TextStyle(
+                                    color: Color(0xFF4A4A4A),
+                                    fontSize: 12,
+                                  ),
+                                ),
+                                const Text('Rs.5000.00',
+                                  style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                ),
+                              ],
+                            ),
+                          ],
                         ),
+                        //for Expense
+                        Row(
+                          children: [
+                            Container(
+                              width: 20,
+                              height: 20,
+                              decoration: BoxDecoration(
+                                color: Color(0xFFD68FE1),
+                                shape: BoxShape.circle,
+                              ),
+                              child: const Center(
+                                child: Icon(Icons.arrow_downward_rounded,
+                                    size: 12,
+                                color: Colors.red,
+                                ),
 
-                        child:  Icon(Icons.arrow_downward_rounded,
-                        size: 12,),
-                      )
-                    ],
+                              ),
+                            ),
+                            SizedBox(width: 10),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  'Expense',
+                                  style: TextStyle(
+                                    color: Color(0xFF4A4A4A),
+                                    fontSize: 12,
+                                  ),
+                                ),
+                                const Text('Rs.2000.00',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
             ),
+            SizedBox(height: 24),
+            //For Transaction & View All
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Transaction",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color:Colors.black,
+                  ),
+                ),
+                Text(
+                  "View All",
+                  style: TextStyle(
+                    fontSize: 16,
+
+                    color: Theme.of(context).colorScheme.tertiary,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 12),
+
           ],
         ),
       ),
