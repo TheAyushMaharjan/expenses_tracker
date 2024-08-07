f80ddcd97ac8ce9f77d1368e344986e4a1cbbfe0
TheAyushMaharjan <ayush.maharjan399@gmail.com>
2024-08-07 18:48:10 +0545

    commit

--- lib/screens/add_expense/views/add_expense.dart
+++ lib/screens/add_expense/views/add_expense.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AddExpense extends StatelessWidget {
@@ -5,12 +6,61 @@ class AddExpense extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Add Expenses'),
-      ),
-      body: const Column(
-        children: [],
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+
+
+        appBar: AppBar(
+        ),
+
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const Text("Add Expenses",style: TextStyle(fontSize: 22),),
+              TextFormField(
+                decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  filled: true,
+                  prefixIcon: Padding(
+                    padding: EdgeInsets.all(12.0),
+                    child: Text(
+                      'Rs.',
+                      style: TextStyle(fontSize: 16.0, color: Colors.grey[600]),
+                    ),
+                  ),
+                  contentPadding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 20.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide.none,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide(color: Colors.grey[300]!),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide(color: Colors.blue),
+                  ),
+                  hintText: 'Enter amount',
+                  hintStyle: TextStyle(color: Colors.grey[400]),
+                ),
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.black,
+                ),
+              ),
+
+              SizedBox(height: 16.0),
+              TextFormField(),
+              SizedBox(height: 16.0),
+              TextFormField(),
+              SizedBox(height: 16.0),
+              TextButton(onPressed: (){}, child: Text("Save")),
+            ],
+          ),
+        ),
       ),
     );
   }
