c88482f6fa06f3da4c7e00287f7763b4c92d8121
Sizan <mahatosizan@gmail.com>
2025-02-13 12:15:34 +0545

    login updated

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,6 @@
 import 'package:expenses_tracker/screens/Login/LoginPage.dart';
+import 'package:expenses_tracker/screens/home/views/home_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart'; // Import the LoginPage widget
 import 'package:get/get.dart'; // Import GetX package
 
@@ -11,14 +13,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp( // Use GetMaterialApp instead of MaterialApp
+    return GetMaterialApp(
       title: 'My Flutter App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-        debugShowCheckedModeBanner: false,
-      home: const LoginPage(), // Directly navigate to LoginPage
+      debugShowCheckedModeBanner: false,
+      home: AuthWrapper(), // Check user authentication
+    );
+  }
+}
+
+class AuthWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          if (snapshot.hasData) {
+            return HomeScreen(); // If logged in, go to HomePage
+          }
+          return LoginPage(); // Otherwise, stay on LoginPage
+        }
+        return const Center(child: CircularProgressIndicator());
+      },
     );
   }
 }
